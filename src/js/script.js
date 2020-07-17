0819bd3c0c62de999a1eace25988b4ad24e21e34
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-17 17:44:23 +0530

    dispatch.html make Draggable

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    $("#DayOne")
+        .sortable({
+            revert: true
+        });
+    $("#DayTwo")
+        .sortable({
+            revert: true
+        });
+    $("#DayThree")
+        .sortable({
+            revert: true
+        });
+    $("#DayFour")
+        .sortable({
+            revert: true
+        });
+    $("#DayFive")
+        .sortable({
+            revert: true
+        });
+}); 
