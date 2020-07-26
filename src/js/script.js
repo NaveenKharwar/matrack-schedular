9f20673d9a3f7de6debdfc2aebc5bf48095de7b2
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-26 13:35:27 +0530

    Prodcution Ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,20 @@
-$(function () {
-    $("#DayOne")
-        .sortable({
-            revert: true
-        });
-    $("#DayTwo")
-        .sortable({
-            revert: true
-        });
-    $("#DayThree")
-        .sortable({
-            revert: true
-        });
-    $("#DayFour")
-        .sortable({
-            revert: true
-        });
-    $("#DayFive")
-        .sortable({
-            revert: true
-        });
-}); 
+$(function() {
+  $("#DayOne").sortable({
+    revert: true,
+  });
+  $("#DayTwo").sortable({
+    revert: true,
+  });
+  $("#DayThree").sortable({
+    revert: true,
+  });
+  $("#DayFour").sortable({
+    revert: true,
+  });
+  $("#DayFive").sortable({
+    revert: true,
+  });
+  $('#driver-name').resizable({
+    handles: 'e, w'
+  });
+});
